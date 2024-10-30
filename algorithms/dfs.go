1f08a856b0d40863b8e0c3e1b26221c24333e847
pemiinem <190139323@aluno.unb.br>
2024-10-30 13:18:09 -0300

    feat: implement DFS algorithm for maze generation

---
+++ algorithms/dfs.go
@@ -0,0 +1,20 @@
+package algorithms
+
+func (m *Maze) GenerateDFS() {
+	stack := [][2]int{{0, 0}}
+	m.grid[0][0].visited = true
+
+	for len(stack) > 0 {
+		current := stack[len(stack)-1]
+		neighbors := m.getUnvisitedNeighbors(current[0], current[1])
+
+		if len(neighbors) > 0 {
+			next := neighbors[m.rng.Intn(len(neighbors))]
+			m.removeWall(current, next)
+			stack = append(stack, next)
+			m.grid[next[0]][next[1]].visited = true
+		} else {
+			stack = stack[:len(stack)-1]
+		}
+	}
+}
