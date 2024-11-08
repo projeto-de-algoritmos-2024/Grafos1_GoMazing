119db0bdb1b8f6654aea99ccc8ea98a91e7f0ef9
pemiinem <190139323@aluno.unb.br>
2024-11-08 17:12:49 -0300

    feat: add time handler for bfs

--- gomazing-frontend/src/MazeGeneration.js
+++ gomazing-frontend/src/MazeGeneration.js
@@ -2,19 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MazeGeneration.css';
 
-const TILE_SIZE = 50;
+const TILE_SIZE = 20;
 
 const MazeGeneration = () => {
     const [mazeSteps, setMazeSteps] = useState([]);
     const [currentStep, setCurrentStep] = useState(0);
-    const [algorithm, setAlgorithm] = useState(1); // Default to DFS
+    const [algorithm, setAlgorithm] = useState(null); // No default algorithm
 
     useEffect(() => {
-        generateMaze();
+        if (algorithm !== null) {
+            generateMaze();
+        }
     }, [algorithm]);
 
     useEffect(() => {
         if (mazeSteps.length > 0) {
+            const intervalTime = algorithm === 2 ? 5000 / mazeSteps.length : 15000 / mazeSteps.length; // Faster for BFS
             const interval = setInterval(() => {
                 setCurrentStep((prevStep) => {
                     if (prevStep < mazeSteps.length - 1) {
@@ -24,15 +27,15 @@ const MazeGeneration = () => {
                         return prevStep;
                     }
                 });
-            }, 15000 / mazeSteps.length);
+            }, intervalTime);
         }
-    }, [mazeSteps]);
+    }, [mazeSteps, algorithm]);
 
     const generateMaze = async () => {
         try {
             const response = await axios.post('http://localhost:8080/generate', {
-                width: 24,
-                height: 18,
+                width: 60,
+                height: 30,
                 algo: algorithm
             });
             console.log("Maze generation response:", response.data); // Debugging line
@@ -45,9 +48,11 @@ const MazeGeneration = () => {
     return (
         <div className="maze-container">
             <div className="controls">
-                <button onClick={() => setAlgorithm(1)}>Generate with DFS</button>
-                <button onClick={() => setAlgorithm(2)}>Generate with BFS</button>
-                <button onClick={() => setAlgorithm(3)}>Generate with Prim's</button>
+                <button onClick={() => setAlgorithm(1)}>Select DFS</button>
+                <button onClick={() => setAlgorithm(2)}>Select BFS</button>
+                <button onClick={() => setAlgorithm(3)}>Select Prim's</button>
+                <button onClick={() => setAlgorithm(4)}>Select Kruskal's</button>
+                <button onClick={generateMaze} disabled={algorithm === null}>Generate Maze</button>
             </div>
             {mazeSteps.length > 0 && mazeSteps[currentStep].map((row, rowIndex) =>
                 Array.isArray(row) ? row.map((cell, cellIndex) => (
