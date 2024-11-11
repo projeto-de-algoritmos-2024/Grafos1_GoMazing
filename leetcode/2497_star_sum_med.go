537640f547528b74ad2dfa9aa55596b760e5ffca
pemiinem <190139323@aluno.unb.br>
2024-11-11 12:42:55 -0300

    chore: accepted solution 2494

--- leetcode/2497_star_sum_med.go
+++ leetcode/2497_star_sum_med.go
@@ -1,61 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 )
 
+// https://leetcode.com/problems/maximum-star-sum-of-a-graph/
 func maxStarSum(vals []int, edges [][]int, k int) int {
 	n := len(vals)
-	adj := make([][]int, n)
 
+	graph := make([][]int, n)
 	for _, edge := range edges {
 		a, b := edge[0], edge[1]
-		adj[a] = append(adj[a], b)
-		adj[b] = append(adj[b], a)
+		graph[a] = append(graph[a], b)
+		graph[b] = append(graph[b], a)
 	}
 
-	maxSum := vals[0]
+	maxStarSum := vals[0]
 
 	for i := 0; i < n; i++ {
-		neighbors := adj[i]
-		sort.Slice(neighbors, func(a, b int) bool {
-			return vals[neighbors[a]] > vals[neighbors[b]]
+
+		var neighborVals []int
+		for _, neighbor := range graph[i] {
+			neighborVals = append(neighborVals, vals[neighbor])
+		}
+
+		sort.Slice(neighborVals, func(a, b int) bool {
+			return neighborVals[a] > neighborVals[b]
 		})
 
-		starSum := vals[i]
-		for j := 0; j < k && j < len(neighbors); j++ {
-			if vals[neighbors[j]] > 0 {
-				starSum += vals[neighbors[j]]
+		currentSum := vals[i]
+
+		for j := 0; j < k && j < len(neighborVals); j++ {
+			if neighborVals[j] > 0 {
+				currentSum += neighborVals[j]
 			}
 		}
 
-		if starSum > maxSum {
-			maxSum = starSum
+		if currentSum > maxStarSum {
+			maxStarSum = currentSum
 		}
 	}
 
-	return maxSum
-}
-
-func mainLeet() {
-	var n, m, k int
-	fmt.Scan(&n, &m, &k)
-
-	vals := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&vals[i])
-	}
-
-	edges := make([][]int, m)
-	for i := 0; i < m; i++ {
-		edges[i] = make([]int, 2)
-		fmt.Scan(&edges[i][0], &edges[i][1])
-	}
-
-	fmt.Println(maxStarSum(vals, edges, k))
-}
-
-func main() {
-	mainLeet()
+	return maxStarSum
 }
