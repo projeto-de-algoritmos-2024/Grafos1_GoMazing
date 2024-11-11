222ad0d1e0c872c5635ea218cc1f8eb0b90949bf
pemiinem <190139323@aluno.unb.br>
2024-11-11 12:47:49 -0300

    chore: first submission run time error

---
+++ leetcode/2876_visited_nodes_hd.py
@@ -0,0 +1,39 @@
+def countVisitedNodes(edges):
+    n = len(edges)
+    answer = [-1] * n
+    visited = [-1] * n
+
+    def dfs(node):
+        if visited[node] == 1:
+            return answer[node]
+        if visited[node] == 0:
+
+            cycle_nodes = []
+            current = node
+            while True:
+                cycle_nodes.append(current)
+                current = edges[current]
+                if current == node:
+                    break
+            cycle_length = len(cycle_nodes)
+            for n in cycle_nodes:
+                answer[n] = cycle_length
+            return cycle_length
+
+        visited[node] = 0
+
+        if edges[node] != -1:
+            cycle_length = dfs(edges[node])
+            if answer[node] == -1:
+                answer[node] = cycle_length + 1 if cycle_length > 0 else -1
+        else:
+            answer[node] = 1
+
+        visited[node] = 1
+        return answer[node]
+
+    for i in range(n):
+        if visited[i] == -1:
+            dfs(i)
+
+    return answer
